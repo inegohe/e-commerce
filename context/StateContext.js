79067afe692711cbfd86f0a81d2840a973fc3ee3
inegohe <ikhosler@gmail.com>
2024-09-20 08:30:31 +0300

    rectify product variable name when incrementing

--- context/StateContext.js
+++ context/StateContext.js
@@ -53,12 +53,12 @@ export const StateContext = ({ children }) => {
     const newCartItems = cartItems.filter((item) => item._id !== id);
 
     if(value == 'inc'){
-      setCartItems([ ...newCartItems, { ...foundProduct, quantity: product.quantity + 1 }])
+      setCartItems([ ...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }])
       setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
       setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1)
     } else if ( value === 'dec')
       if(foundProduct.quantity > 1){
-        setCartItems([ ...newCartItems, { ...foundProduct, quantity: product.quantity - 1 }])
+        setCartItems([ ...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 }])
         setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
         setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1)  
       }
